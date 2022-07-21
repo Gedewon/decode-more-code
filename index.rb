d77a31554e77a4dfa5ffedd3fd6b9e799ce40fe4
gedewon <gedewon.hayle.9@gmail.com>
2022-07-21 21:38:17 +0300

    Fix ruby lint

--- index.rb
+++ index.rb
@@ -1,55 +1,28 @@
-$MORSE_CODE_TO_LETTER = {
-   '.-' => 'a',
-   '-...'=> 'b',
-   '-.-.' => 'c',
-   '-..' => 'd',
-   '.' => 'e',
-   '..-.'=> 'f',
-   '--.'=> 'g',
-   '....'=> 'h',
-   '..'=>'i',
-   '.---'=>'j',
-   '-.-'=>'k',
-   '.-..'=>'l',
-   '--'=>'m',
-   '-.'=>'n',
-   '---'=>'o',
-   '.--.'=>'p',
-   '--.-'=>'q',
-   '.-.'=>'r',
-   '...'=>'s',
-   '-' => 't',
-   '..-' => 'u',
-   '...-' => 'v',
-   '.--' => 'w',
-   '-..-'=>'x',
-   '-.--'=>'y',
-   '--..' => 'z'
-}
-
 def decode_char(input)
-   return "#{$MORSE_CODE_TO_LETTER[input].upcase}"
+  morse_code_to_letter = { '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f',
+                           '--.' => 'g', '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l',
+                           '--' => 'm', '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r',
+                           '...' => 's', '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x',
+                           '-.--' => 'y', '--..' => 'z' }
+  morse_code_to_letter[input].upcase
 end
 
 def decode_word(input)
-   arrayOfCodes = input.split(' ')
-   decodedString =[]
-    arrayOfCodes.each do |element|
-      decodedString.push(decode_char(element))
-    end
-   return decodedString.join('').upcase
+  array_of_codes = input.split
+  decoded_string = []
+  array_of_codes.each do |element|
+    decoded_string.push(decode_char(element))
+  end
+  decoded_string.join
 end
 
 def decode(input)
-   arrayOfWords= input.split('   ')
-   decodedString =[]
-    arrayOfWords.each do |element|
-      decodedString.push(decode_word(element))
-    end
-   puts decodedString.join(' ').upcase
+  array_of_words = input.split('   ')
+  decoded_string = []
+  array_of_words.each do |element|
+    decoded_string.push(decode_word(element))
+  end
+  puts decoded_string.join(' ')
 end
 
-
-puts decode_char (".-")
-puts decode("-- -.--   -. .- -- .")
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
